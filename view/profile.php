6e0f645a06d311e2b423e6fd32b254a69857800d
TatsanaiJoez007 <tatsanai.n@rmutsvmail.com>
2024-05-12 22:22:44 +0700

    profile

--- view/profile.php
+++ view/profile.php
@@ -206,7 +206,8 @@ if (!isset($_SESSION['login'])) {
                 <p class="text-muted mb-0">example@example.com</p>
               </div>
             </div>
-           
+            
+            <hr>
             <div class="row">
               <div class="col-sm-3">
                 <p class="mb-0">Mobile</p>
