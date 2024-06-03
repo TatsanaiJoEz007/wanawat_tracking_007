b0b1784b21c0fd6e1cfe9b9af91fbc0e6fca3503
Tagwithsaltedcaramel <koontackch@gmail.com>
2024-06-04 01:10:08 +0700

    Update reset password button type in permission_user.php and add email display in contact.php

--- view/admin/permission_user.php
+++ view/admin/permission_user.php
@@ -550,7 +550,7 @@ $query = mysqli_query($conn, $sql);
                             <input type="password" class="form-control" id="confirm-password" name="confirmPassword" required placeholder="Confirm new password">
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="reset-password-btn btn btn-success">ยืนยันการเปลี่ยนรหัสผ่าน</button>
+                            <button type="button" class="reset-password-btn btn btn-success">ยืนยันการเปลี่ยนรหัสผ่าน</button>
 
                         </div>
                     </form>
