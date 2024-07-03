123505a46267484d27a888e859bae056b9bec719
TatsanaiJoez007 <tatsanai.n@rmutsvmail.com>
2024-07-04 01:35:26 +0700

    ค่อยทำต่อละกานนนน

--- view/orderhistory.php
+++ view/orderhistory.php
@@ -1,11 +1,17 @@
 
-<?php require_once ('config/connect.php'); ?>
+<?php 
+require_once ('config/connect.php'); 
+session_start();
+
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="th">
 
 <head>
-    <title>Oderhistory</title>
+    <title>Order history</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
@@ -59,8 +65,9 @@
 
 <?php require_once('function/navindex.php'); ?>
 <body>
-
-    <h1 class="app-page-title">ประวัติการสั่งซื้อ</h1>
+<br>
+<br>
+ <h1 class="app-page-title">    &nbsp;&nbsp;ประวัติการสั่งซื้อ</h1>
     <hr class="mb-4">
     <div class="container">
         <div class="row g-4 settings-section">
@@ -74,16 +81,53 @@
                             <table class="table table-striped" id="Tableall">
                                 <thead>
                                     <tr>
-                                        <th scope="col" style="text-align: center;">เลขคำสั่งซื้อ</th>
+                                        <th scope="col" style="text-align: center;">#</th>
+                                        <th scope="col" style="text-align: center;">หมายเลขบิล</th>
                                         <th scope="col" style="text-align: center;">สินค้า</th>
                                         <th scope="col" style="text-align: center;">สถานะ</th>
                                         <th scope="col" style="text-align: center;">วันที่สั่งซื้อ</th>
                                         <th scope="col" style="text-align: center;">จัดการ</th>
-
-                                        
                                     </tr>
                                 </thead>
                                 <tbody class="text-center">
+                                    <?php
+                                    $sql = "SELECT * FROM tb_line ";
+                                    $result = $conn->query($sql);
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_assoc()) {
+                                            $bill_number = $row["line_bill_number"];
+                                            $sql2 = "SELECT * FROM tb_header WHERE bill_number = '$bill_number'";
+                                            $result2 = $conn->query($sql2);
+                                            $row2 = $result2->fetch_assoc();
+                                            $bill_status = $row2["bill_status"];
+                                            $status = "";
+                                            if ($bill_status == 1) {
+                                                $status = "รอการชำระเงิน";
+                                            } else if ($bill_status == 2) {
+                                                $status = "ชำระเงินแล้ว";
+                                            } else if ($bill_status == 3) {
+                                                $status = "กำลังจัดส่ง";
+                                            } else if ($bill_status == 4) {
+                                                $status = "จัดส่งแล้ว";
+                                            } else if ($bill_status == 5) {
+                                                $status = "ยกเลิก";
+                                            }
+                                            echo "<tr>";
+                                            echo "<td>" . $row["line_id"] . "</td>";
+                                            echo "<td>" . $row["line_bill_number"] . "</td>";
+                                            echo "<td>" . $row["item_desc"] . "</td>";
+                                            echo "<td>" . $status . "</td>";
+                                            echo "<td>" . $row2["bill_date"] . "</td>";
+                                            echo "<td>";
+                                            if ($bill_status == 1) {
+                                                echo "<a href='function/orderhistory/cancelorder.php?line_id={$row['line_id']}' class='btn btn-danger'>ยกเลิก</a>";
+                                            }
+                                            echo "</td>";
+                                            echo "</tr>";
+                                        }
+                                    }
+                                    ?>
+
 
                                 </tbody>
                             </table>
